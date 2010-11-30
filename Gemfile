d2f450c5db2cf845404f89a51ffbc9df0448587c
Irrational Design <services@irrationaldesign.com>
2010-11-30 10:26:58 -0800

    initial

---
+++ Gemfile
@@ -0,0 +1,25 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.3'
+gem 'haml', '~>3.0.18'
+gem 'pg'
+
+gem 'devise', '1.1.2'
+
+gem 'hoptoad_notifier', '2.3.8'
+gem 'newrelic_rpm', '2.13.2'
+gem 'heroku'
+
+group :development do
+  gem 'spork'
+  gem 'mongrel'
+end
+
+group :test, :development do
+  gem 'jasmine'
+  gem 'ruby-debug-ide'
+  gem 'ruby-debug-base'
+  gem 'rspec-rails'
+  gem 'factory_girl_rails', '1.0'
+  gem 'webrat', '0.7.1'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Secretgoals::Application.config.secret_token = 'c229fcc320434c6bfe2584dd497f47923a4b3833e4d2704abc2dd66363667a4f0eb40cc2465f0a1805b4d1aaf990a8bfa63ca008eb3aa6ced6abba776cc8aa25'
