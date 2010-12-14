c1919f9060385e69c3d02d684dbce2cfcc7f0cac
Jared Cosulich <jaredcosulich@Jared-Cosulichs-MacBook-Pro.local>
2010-12-14 15:58:26 -0800

    - fixing bug with going to a bad me_goals/show page

--- spec/controllers/me_goals_controller_spec.rb
+++ spec/controllers/me_goals_controller_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
 describe MeGoalsController do
+  describe "show" do
+    before :each do
+      @user = Factory(:user_goal).user
+      sign_in @user
+    end
+    it "should redirect to index if goal can't be found" do
+      get :show, :id => "bad_id"
+      response.should redirect_to me_goals_path
+    end
 
+    it "should redirect to index if goal can't be found" do
+      other_user_goal = Factory(:user_goal)
+      get :show, :id => other_user_goal.id.to_obfuscated
+      response.should redirect_to me_goals_path
+    end
+  end
 end
