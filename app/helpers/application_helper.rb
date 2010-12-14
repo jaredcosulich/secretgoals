6bf958ed0a6e0b7e4b4c8d1523b809dacff5d827
Irrational Design <services@irrationaldesign.com>
2010-12-13 16:18:25 -0800

    intra_app

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
   def include_clixpy?
-    puts request.referer
-    ENV["CLIXPY"] && request.referer.include?("secretgoals.com")
+    intra_app = request.referer.starts_with?("http://www.secretgoals.com") || request.referer.starts_with?("http://secretgoals.com")
+    puts "Referer: #{request.referer} (intra_app: #{intra_app})"
+    ENV["CLIXPY"] && (intra_app)
   end
 end
