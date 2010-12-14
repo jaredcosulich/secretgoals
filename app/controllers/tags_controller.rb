779b6282d83939e20583efa6c51988cd6f33f8f7
Irrational Design <services@irrationaldesign.com>
2010-12-13 16:28:02 -0800

    - fixing blank tag page

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -6,6 +6,7 @@ class TagsController < ApplicationController
   def show
     @tag = Tag.find_by_permalink!(params[:id])
     @updates = @tag.updates.latest(10)
+    @goals = @tag.goals
   end
 
 end
