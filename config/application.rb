892eb81668857820d53a5af3fb83ba36481c828c
Irrational Design <services@irrationaldesign.com>
2010-12-15 15:34:57 -0800

    change password

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module Secretgoals
       g.test_framework :rspec
       g.helper false
     end
-    
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
