14cca6d6a25a0bc68735c23970ce0aeac2753e33
Jared Cosulich <jaredcosulich@Jared-Cosulichs-MacBook-Pro.local>
2011-01-26 07:35:13 -0800

    - don't show emails in replies

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
     intra_app = request.referer.starts_with?("http://www.secretgoals.com") || request.referer.starts_with?("http://secretgoals.com")
     ENV["CLIXPY"] && (intra_app)
   end
+
+  def remove_emails(text)
+    text.gsub(/\b\w+@\w+\.\w+/, "[private]")
+  end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,6 +1,11 @@
 require 'spec_helper'
 
 describe ApplicationHelper do
-# test Referer: http://www.stumbleupon.com/su/AAWPVQ/www.secretgoals.com/ (intra_app: false)
 
+  describe "remove_emails" do
+    it "should remove any emails from text" do
+      text = "hey person@example.com, this is an email: soandso@example.com and another email test123@test123.ca."
+      remove_emails(text).should == "hey [private], this is an email: [private] and another email [private]."
+    end
+  end
 end
