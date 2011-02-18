6e77d9af9fd8592d462f82f3f46dbe01d6f47d71
Jared Cosulich <jaredcosulich@Jared-Cosulichs-MacBook-Pro.local>
2011-02-18 10:52:33 -0800

    - stats for secret goals

--- app/controllers/admin/goals_controller.rb
+++ app/controllers/admin/goals_controller.rb
@@ -3,6 +3,23 @@ class Admin::GoalsController < ApplicationController
     @tags = Tag.order("title asc").all
     @untagged_goal = Goal.includes(:tags).where("tags.id is null").first
     @goals = Goal.includes(:tags).order("id desc").limit(20)
+    @goal_stats = [
+      {:last_24_hours => UserGoal.where("created_at > ?", 24.hours.ago).count},
+      {:last_3_days => UserGoal.where("created_at > ?", 3.days.ago).count},
+      {:last_week => UserGoal.where("created_at > ?", 1.week.ago).count},
+      {:last_week_24_hours => UserGoal.where("created_at BETWEEN ? AND ?", (1.week + 24.hours).ago, 1.week.ago).count},
+      {:last_week_3_days => UserGoal.where("created_at BETWEEN ? AND ?", (1.week + 3.days).ago, 1.week.ago).count},
+      {:last_week_1_week => UserGoal.where("created_at BETWEEN ? AND ?", 2.weeks.ago, 1.week.ago).count}
+    ]
+
+    @update_stats = [
+      {:last_24_hours => Update.where("created_at > ?", 24.hours.ago).count},
+      {:last_3_days => Update.where("created_at > ?", 3.days.ago).count},
+      {:last_week => Update.where("created_at > ?", 1.week.ago).count},
+      {:last_week_24_hours => Update.where("created_at BETWEEN ? AND ?", (1.week + 24.hours).ago, 1.week.ago).count},
+      {:last_week_3_days => Update.where("created_at BETWEEN ? AND ?", (1.week + 3.days).ago, 1.week.ago).count},
+      {:last_week_1_week => Update.where("created_at BETWEEN ? AND ?", 2.weeks.ago, 1.week.ago).count}  
+    ]
   end
 
   def assign_tags
